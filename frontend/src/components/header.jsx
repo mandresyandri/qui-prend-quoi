86f585397d354836680527be4dd4ac9ab078d674
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2023-12-26 17:04:05 +0000

    Starting the header part

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,9 +1,25 @@
 import "../styles/header.css";
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
     return (
         <header>
-            <p>Mon header</p>
+            <div className="bg-img">
+                <div className="bg-text">
+                    <p className="leadText">
+                        Besoin de créer une liste participative en 5 min ? 
+                    </p>
+                    <h1 className="headingText">Créez une liste collaborative pour</h1> 
+                    {/* Type writter effect */}
+                    <Typewriter 
+                        options={{
+                            strings:["un dîner", "un apéro","un anniversaire"], 
+                            autoStart: true, 
+                            loop: true
+                        }}
+                    />
+                </div>
+            </div>
         </header>
     );
 }
