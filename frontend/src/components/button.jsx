2794eaeb4843f7262bb01de9d98652493db7f8ba
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2023-12-27 10:13:20 +0000

    Ok for heading

---
+++ frontend/src/components/button.jsx
@@ -0,0 +1,9 @@
+import "../styles/button.css";
+
+const ctaButton = () => {
+    return (
+        <div className="ctaButton">Créer une liste gratuitement</div>
+    )
+}
+
+export default ctaButton;
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,23 +1,33 @@
 import "../styles/header.css";
 import Typewriter from 'typewriter-effect';
+import Button from './button';
 
 const Header = () => {
     return (
         <header>
             <div className="bg-img">
                 <div className="bg-text">
-                    <p className="leadText">
-                        Besoin de créer une liste participative en 5 min ? 
-                    </p>
-                    <h1 className="headingText">Créez une liste collaborative pour</h1> 
-                    {/* Type writter effect */}
-                    <Typewriter 
-                        options={{
-                            strings:["un dîner", "un apéro","un anniversaire"], 
-                            autoStart: true, 
-                            loop: true
-                        }}
-                    />
+                    <div className="textContent">
+                        <p className="leadText">
+                            Besoin de créer une liste participative en 5 min ? 
+                        </p>
+                        <div className="h1Text">
+                            <h1 className="headingText">
+                                Créez une liste collaborative pour&nbsp;
+                            </h1> 
+                            {/* Typewritter effect */}
+                            <Typewriter 
+                                classname="headingTextdynamic"
+                                options={{
+                                    strings:["un dîner", "un apéro", "un anniversaire"], 
+                                    autoStart: true, 
+                                    loop: true
+                                }}
+                            />
+                        </div>
+                        <Button className="headingButton"/>
+                        <p className="leadText bottomText">S'occuper d'une liste n'a jamais été aussi simple !</p>
+                    </div>
                 </div>
             </div>
         </header>
